c17e3cdb733edfbe10389d161b2eea32c5b08135
David Paul <david-paul23@hotmail.com>
2016-11-18 15:53:42 +0000

    init development

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import React, {Component} from 'react';
+import NavBar from './NavBar.jsx'
 
 class App extends Component {
+// initial state
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: {name: ""},
+      availableRoutes: [],
+      userLocation: { currentLocation:[], destination:[]},
+      routeCount: 0,
+    };
+  }
+
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+          <nav>
+            <span className='userCount'>{this.state.userCount} users online</span>
+            <h1>Chatty</h1>
+          </nav>
+      </div>
     );
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -8,4 +8,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
+window.render = () => {
 ReactDOM.render(<App />, document.getElementById('react-root'));
+}
+render();
