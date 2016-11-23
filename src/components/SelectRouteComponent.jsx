3067e45aa5f483fe2f2df89e56a1982dfc1b15ea
Clive Yip <yipsifu@gmail.com>
2016-11-23 19:53:35 +0000

    got select route option to set state of currentSelectedRoute

--- src/components/SelectRouteComponent.jsx
+++ src/components/SelectRouteComponent.jsx
@@ -4,20 +4,16 @@ import {FormGroup, ControlLabel, FormControl, Button} from 'react-bootstrap';
 class SelectRouteComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: 'coconut'};
 
-    this.state.RouteStations =
-    {
-      LakeshoreWest: ['Union Station, Clarkson, Oakville, Bronte, Burlington'],
-      Milton: ['Union Station, Kipling, Dixie, Streetsville, Meadowvale, Lisgar, Milton'],
-      Kitchener: ['Kitchener Bus Terminal', 'University of Guelph', 'Yorkdale Bus Terminal', 'Union Station Bus Terminal']
-    };
-
-    // {
-    //   LakeshoreWest: {'Union Station, Clarkson, Oakville, Bronte, Burlington'},
-    //   Milton: {'Union Station, Kipling, Dixie, Streetsville, Meadowvale, Lisgar, Milton'},
-    //   Kitchener: {'Kitchener Bus Terminal', 'University of Guelph', 'Yorkdale Bus Terminal', 'Union Station Bus Terminal'}
-    // };
+    this.state = {
+      currentSelectedRoute: 'current',
+      RouteStations:
+        {
+          LakeshoreWest: ['Union Station', 'Clarkson', 'Oakville', 'Bronte', 'Burlington'],
+          Milton: ['Union Station', 'Kipling', 'Dixie', 'Streetsville', 'Meadowvale', 'Lisgar', 'Milton'],
+          Kitchener: ['Kitchener Bus Terminal', 'University of Guelph', 'Yorkdale Bus Terminal', 'Union Station Bus Terminal']
+        }
+    }
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -25,6 +21,7 @@ class SelectRouteComponent extends Component {
     this.getRoutes = this.getRoutes.bind(this);
   }
 
+  // helper function to get Routes from RouteStation data
   getRoutes() {
     var routes = [];
     for (var i in this.state.RouteStations) {
@@ -34,16 +31,17 @@ class SelectRouteComponent extends Component {
     return routes
   }
 
+  // iterate over Routes array and create html <option> elements
   renderOptions(items) {
-    return items.map(item => <option key={item} value="other">{item}</option>)
+    return items.map(item => <option key={item} value={item}>{item}</option>)
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({currentSelectedRoute: event.target.value});
   }
 
   handleSubmit(event) {
-    alert('Selected Route: ' + this.state.value);
+    alert('Selected Route: ' + this.state.currentSelectedRoute);
     event.preventDefault();
   }
 
@@ -52,8 +50,8 @@ class SelectRouteComponent extends Component {
       <form onSubmit={this.handleSubmit}>
         <label>
           Select Route:
-          <select value={this.state.value} onChange={this.handleChange}>
-            { this.renderOptions(this.getRoutes(this.state.RouteStations)) }
+          <select value={this.state.currentSelectedRoute} onChange={this.handleChange}>
+            { this.renderOptions(this.getRoutes()) }
           </select>
         </label>
         <input type="submit" value="Submit" />
