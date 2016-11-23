87c1100e0d58a8d54cc0775bd4b1bce54c467d3f
Clive Yip <yipsifu@gmail.com>
2016-11-23 02:44:31 +0000

    install react-bootstrap-date-picker and add component

--- src/components/date.jsx
+++ src/components/date.jsx
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
 import {Well} from 'react-bootstrap';
 import {bsStyle} from 'react-bootstrap';
+var DatePicker = require("react-bootstrap-date-picker");
 
 class Date extends Component {
   render() {
     return (
       <div>
-<Well><i className="fa fa-calendar-check-o" aria-hidden="true"></i> Travelling <strong>November 18</strong></Well>
+        <Well>
+          <i className="fa fa-calendar-check-o" aria-hidden="true"></i>
+          Travelling
+          <DatePicker id="example-datepicker" />
+        </Well>
 
       </div>
     );
