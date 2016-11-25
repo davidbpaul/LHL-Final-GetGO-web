7d7606d20559a6609beb809f13a2cbaf1ad0ff40
Clive Yip <yipsifu@gmail.com>
2016-11-25 20:34:44 +0000

    refractor and bug fix

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,10 @@
 import { combineReducers } from 'redux';
 import route from './route.js';
 import location from './location.js';
-import destination from './destination.js';
 // import activeRoutes from 'reducerActiveRoutes.js'
 
 const rootReducer = combineReducers({
   route,
   location,
-  destination
-  // activeRoutes: activeRoutes
-
 })
 export default rootReducer;
